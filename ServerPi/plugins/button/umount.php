930042ae30f4fe77094f9e62357fd3da64f66f16
Dylan Gageot <gageot.dylan@gmail.com>
2016-02-17 22:27:43 +0100

    Update umount.php

--- ServerPi/plugins/button/umount.php
+++ ServerPi/plugins/button/umount.php
@@ -1,7 +1,7 @@
 <?php
 	include('../../config.php');	
 	exec('sudo /bin/umount ' . exec('sudo fdisk -l | grep /dev/sd | tail -n 1 | cut -d " " -f 1'));
-	rmdir($_SERVER['DOCUMENT_ROOT'] . '/usb/');	
+	rmdir($ADRESS . '/usb/');	
 	header('Location: '. $URL);
 	exit;
 ?>
