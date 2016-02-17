82f50b0da0fccff4e5d5176572f47ab3ea6501c5
Dylan Gageot <gageot.dylan@gmail.com>
2016-02-17 22:23:39 +0100

    Update config.php

--- ServerPi/config.php
+++ ServerPi/config.php
@@ -4,6 +4,6 @@
 	# Set by the designer to recognize the version of ServerPi.
 	$REVISION      = 'rev_1';
 	# Please be sure that the path and the url below are correct:
-	$ADRESS        = '/var/www/html';
+	$ADRESS        = $_SERVER['DOCUMENT_ROOT'];
 	$URL           = 'http://'. $_SERVER['SERVER_NAME'];
 ?>
