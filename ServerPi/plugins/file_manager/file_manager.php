069ac4697e8b492c65e9ddfc71f2949b4c0920c9
Dylan Gageot <gageot.dylan@gmail.com>
2016-02-17 22:19:22 +0100

    Update file_manager.php

--- ServerPi/plugins/file_manager/file_manager.php
+++ ServerPi/plugins/file_manager/file_manager.php
@@ -1,4 +1,5 @@
 <?php
+	// function found here : http://stackoverflow.com/questions/5501451/php-x86-how-to-get-filesize-of-2-gb-file-without-external-program/5504829#5504829
 	function filesize64($file)
 	{
 		static $iswin;
@@ -11,7 +12,7 @@
 			$exec_works = (function_exists('exec') && !ini_get('safe_mode') && @exec('echo EXEC') == 'EXEC');
 		}
 
-				// try a shell command
+		// try a shell command
 		if ($exec_works) {
 			$cmd = ($iswin) ? "for %F in (\"$file\") do @echo %~zF" : "stat -c%s \"$file\"";
 			@exec($cmd, $output);
@@ -50,8 +51,7 @@
 
 	$dir = "./" . $_GET["dir"];
 	// Test if the directory exist.
-	if (is_dir($dir))
-	{
+	if (is_dir($dir)) {
 		echo '<table id="list"><tbody>';
 		if ($_GET["dir"] == '' || $_GET["dir"] == "./" || $_GET["dir"] == "/") { 
 			echo '<tr><th>Name - /</th><th>Size</th><th width=10></th></tr>';
@@ -64,27 +64,23 @@
 			echo '<tr><th>Name - /'. $_GET["dir"] .'</th><th>Size</th><th width=10></th></tr>';
 			echo '<tr><td><a id="file" href="?dir='. dirname($_GET["dir"]) .'/">..</a></td><td WIDTH=81>-</td><td width=10></td></tr>'; 
 		}
-		if ($dh = opendir($dir)) 
-		{
-			while (($file = readdir($dh)) !== false) 
-			{
-				if( $file != '.' && $file != '..' && $file != 'index.php' && $file != 'ServerPi') 
-				{
-					if(filetype($dir . $file) == "dir") // Test the type of file (file or dir)
-					{
+		if ($dir_opened = opendir($dir)) {
+			while (($file = readdir($dir_opened)) !== false) {
+				if( $file != '.' && $file != '..' && $file != 'index.php' && $file != 'ServerPi') {
+					if(filetype($dir . $file) == "dir") {
 						echo '<tr><td><a id="file" href="?dir='. $_GET["dir"] . $file .'/">' . $file . '/</a></td><td WIDTH=81>-</td><td width=10><a id="file" title="Remove this dir" href="ServerPi/plugins/file_manager/remove.php?adress='. urlencode(dirname(dirname(dirname(__DIR__))) . '/' . $_GET["dir"] . $file) .'/" onclick="return(confirm(\'Are you sure to delete this directory ?\'));">×</a></td></tr>';
 					}
-					else
-					{
+					else {
 						$size = filesize_char($ADRESS . '/' . $_GET["dir"] . $file);
 						echo '<tr><td><a id="file" href="' .$_GET["dir"]. $file . '">' . $file . '</a></td><td WIDTH=81>'. $size . '</td><td width=10><a id="file" title="Remove this file" href="ServerPi/plugins/file_manager/remove.php?adress='. urlencode(dirname(dirname(dirname(__DIR__))) . '/' . $_GET["dir"] . $file) . '" onclick="return(confirm(\'Are you sure to delete this file ?\'));">×</a></td></tr>';
 					}
 				}
 			}
-			closedir($dh);
+			closedir($dir_opened);
 			echo '</tbody></table>';
 		}
 	}
-	else
+	else {
 		echo '<table id="list"><tr><th>Dossier introuvable</th></tr></table>';
+	}
 ?>
