5c28c78d3b5ef85b988ee9947a294c03e2e0a020
Dylan Gageot <gageot.dylan@gmail.com>
2016-02-17 22:28:16 +0100

    Update mount.php

--- ServerPi/plugins/button/mount.php
+++ ServerPi/plugins/button/mount.php
@@ -1,7 +1,7 @@
 <?php
 	include('../../config.php');
 	mkdir($ADRESS . '/usb/');
-	exec('sudo /bin/mount ' . exec('sudo fdisk -l | grep /dev/sd | tail -n 1 | cut -d " " -f 1') . ' ' . $_SERVER['DOCUMENT_ROOT'] . '/usb -o umask=000');
+	exec('sudo /bin/mount ' . exec('sudo fdisk -l | grep /dev/sd | tail -n 1 | cut -d " " -f 1') . ' ' . $ADRESS . '/usb -o umask=000');
 	header('Location: '. $URL);
 	exit;
 ?>
