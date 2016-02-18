7723658abeb17fa66857719fa24eab4bf7e1eefa
Dylan Gageot <gageot.dylan@gmail.com>
2016-02-18 20:14:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,27 @@
 		<title><?php echo $TITLE; ?></title>
 		<link rel="icon" href="ServerPi/images/favicon.png" type="image/x-ico">
 		<link rel="stylesheet" href="ServerPi/theme.css" type="text/css">
+		<?php 	$plugins_ref = fopen("ServerPi/plugins/plugins", "r");
+			if ($plugins_ref) {
+				while (!feof($plugins_ref)) {
+					$buffer = fgets($plugins_ref);
+					echo '<link rel="stylesheet" href="ServerPi/plugins/'. $buffer .'/style.css" type="text/css">';
+				}
+				fclose($plugins_ref);
+			}
+		?>
 	</head>
 	<body>
 		<div id="logo"></div>
-		<?php include('ServerPi/plugins/plugins.php'); ?>
+		<?php 	$plugins_ref = fopen("ServerPi/plugins/plugins", "r");
+			if ($plugins_ref) {
+				while (!feof($plugins_ref)) {
+					$buffer = fgets($plugins_ref);
+					include('ServerPi/plugins/'. $buffer .'/'. $buffer .'.php');
+				}
+				fclose($plugins_ref);
+			}
+		?>
 		<div id="footer"><p><?php echo $REVISION; ?> - Made by Naipsys - 2016</p></div>
 	</body>
 </html>
