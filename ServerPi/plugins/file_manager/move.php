84743021f347018d301b693c83d1061cd1d40c7d
Dylan Gageot <gageot.dylan@gmail.com>
2016-02-18 16:39:35 +0100

    Update move.php

--- ServerPi/plugins/file_manager/move.php
+++ ServerPi/plugins/file_manager/move.php
@@ -2,7 +2,7 @@
 	include('../../config.php');
 	$path = $_POST['path'];
 	$new_path = $_POST['new_path'];
-	$dir_file_name = ($path, dirname($path);
+	$dir_file_name = substr($path, dirname($path));
 	if (rename($path, $ADRESS . $new_path . $dir_file_name)) {
 		$adress = $URL .'/index.php?dir='. substr(dirname($path), strlen($ADRESS) + 1) .'/';
 		header('Location: '. $adress);
