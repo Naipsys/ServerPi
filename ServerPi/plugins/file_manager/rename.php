0bc14c9942996a54e11175d8f1a686d37345f088
Dylan Gageot <gageot.dylan@gmail.com>
2016-02-18 16:25:53 +0100

    Update rename.php

--- ServerPi/plugins/file_manager/rename.php
+++ ServerPi/plugins/file_manager/rename.php
@@ -1,7 +1,7 @@
 <?php
 	$path = $_POST['path'];
 	$new_name = $_POST['new_name'];
-	if (rename($path, dirname($path) . '/' . $new_name) {
+	if (rename($path, dirname($path) . '/' . $new_name)) {
 		$adress = $URL .'/index.php?dir='. substr(dirname($path), strlen($ADRESS) + 1) .'/';
 		header('Location: '. $adress);
 		exit;
