32e02f920a69ae4a6c57a5abb0c0882470ab3ee0
Dylan Gageot <gageot.dylan@gmail.com>
2016-03-08 14:44:26 +0100

    Update french.php

--- ServerPi/plugins/file_manager/languages/french.php
+++ ServerPi/plugins/file_manager/languages/french.php
@@ -4,8 +4,8 @@
 	$LANGUAGE_FILE_MANAGER['new_name']			= "Nouveau nom";
 	$LANGUAGE_FILE_MANAGER['rename']			= "Renommer";
 	$LANGUAGE_FILE_MANAGER['move'] 				= "Déplacer";
-	$LANGUAGE_FILE_MANAGER['copy']      		= "Copier";
+	$LANGUAGE_FILE_MANAGER['copy']      			= "Copier";
 	$LANGUAGE_FILE_MANAGER['delete']			= "Supprimer";
 	$LANGUAGE_FILE_MANAGER['close'] 			= "Fermer";
-	$LANGUAGE_FILE_MANAGER['error_directory'] 	= "Dossier introuvable";
+	$LANGUAGE_FILE_MANAGER['error_directory'] 		= "Dossier introuvable";
 ?>
