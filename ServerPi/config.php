de61e30200507534bed118236432d7795c399fa5
Dylan Gageot <gageot.dylan@gmail.com>
2016-03-08 14:43:55 +0100

    Update config.php

--- ServerPi/config.php
+++ ServerPi/config.php
@@ -4,7 +4,7 @@
 	# Set by the designer to recognize the version of ServerPi.
 	$REVISION      = 'rev_1';
 	# Language to load.
-	$LANGUAGE      = 'french';
+	$LANGUAGE      = 'english';
 	# Please be sure that the path and the url below are correct:
 	$ADRESS        = $_SERVER['DOCUMENT_ROOT'];
 	$URL           = 'http://'. $_SERVER['SERVER_NAME'];
